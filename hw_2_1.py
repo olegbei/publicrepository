c518fcb58edd5ae7c39f4539c6c00680ab53eb34
Oleg <oleg.bei777@gmail.com>
2019-02-17 13:49:54 +0200

    improved

--- hw_2_1.py
+++ hw_2_1.py
@@ -1,4 +1,3 @@
-
 a = float(input("Please enter a number 1"))
 b = float(input("Please enter a number 2"))
 
@@ -8,14 +7,14 @@ if a < b:
     if float(a)> int(a): # обрізаємо десяті
         a = int(a)
         b = int(b)
-        for i in range(a + 1, b + 1):
+        for i in range(b + 1):
             sum += i
             print('Оце ціле число входить', i)
 
     else:
         a = int(a)
         b = int(b)
-        for i in range(a, b + 1):
+        for i in range(b + 1):
             sum += i
             print('Оце ціле число входить', i)
 
@@ -28,28 +27,53 @@ elif a > b:
     if float(b) > int(b): #обрізаємо десяті
         x = int(b) # вводимо нові переміні, для їх заміни
         y = int(a)
-        for i in range(x + 1, y + 1):
+        for i in range(y + 1):
             sum += i
             print('Оце ціле число входить', i)
 
     else:
         x = int(b)# вводимо нові переміні, для їх заміни
         y = int(a)
-        for i in range(x, y + 1):
+        for i in range(y + 1):
             sum += i
             print('Оце ціле число входить', i)
 
 print("Сума двох натуральних чисел", sum)
 
 
-# while not a.isdigit():
-#     print('Please enter an integer number and not a string or float number')
-#     a = input("Please enter a number 1")
-#
-#
-# while not b.isdigit():
-#     print('Please enter an integer number and not a string or float number')
-#     b = input("Please enter a number 2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
